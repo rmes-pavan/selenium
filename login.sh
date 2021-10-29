222e681f41a1d6b9e0ddeabcf41661a455f5a187
rmes-pavan <92154998+rmes-pavan@users.noreply.github.com>
2021-10-29 18:07:39 +0530

    Update login.sh

--- login.sh
+++ login.sh
@@ -3,7 +3,9 @@
 virtualenv login
 source login/bin/activate
 mv login.py login/
-mv login.sh login/
-python3 login/login.py
+cd login/
+pip3 install selenium
+python3 login.py
 cd
 rm -r login
+rm login.sh
