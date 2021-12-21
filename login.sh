6e85c96ab84c518693ddf79cdbac9453d0050983
rmes-pavan <92154998+rmes-pavan@users.noreply.github.com>
2021-12-21 19:05:39 +0530

    Update login.sh

--- login.sh
+++ login.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-virtualenv login
-source login/bin/activate
-mv selTest login/
-cd login/
-pip3 -q install selenium
- python3 selTest/tests/login.py
-cd
-rm -r login
-rm login.sh
+# virtualenv login
+# source login/bin/activate
+# mv selTest login/
+cd sel/
+# pip3 -q install selenium
+python3 selTest/tests/alarm_test.py
+rm -r selTest
+rm test.sh
 
