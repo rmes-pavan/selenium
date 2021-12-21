4cf3cd30691982bb330d439cb0f8e2cc378686a8
rmes-pavan <92154998+rmes-pavan@users.noreply.github.com>
2021-12-21 19:54:44 +0530

    Update testScript.sh

--- testScript.sh
+++ testScript.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+pwd
 cd sel/
-# virtualenv login
+#virtualenv login
 source bin/activate
 # mv selTest login/
 # pip3 -q install selenium
-python3 selTest/tests/alarm_test.py
-# rm -r selTest
+cd selTest/tests
+python3 alarm_test.py
+cd ..
+cd reports
+pwd
+echo 'testsim@123' | sudo -S cp Alarm_test.html /var/www/testReports/
+echo 'testsim@123' | sudo -S sudo service nginx restart
+rm -r selTest
 rm testScript.sh
 
